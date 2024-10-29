4b091cf08876c91d0b29c22838fd59cf736e14e5
JakelineFM <jaki510fame@gmail.com>
2024-10-29 14:03:36 -0400

    Update sketch.js
    I stayed with the glowing ball idea, and I tranformed into a class with the function of reducing the glow. Also, intead of other glowing balls I changed for triangles, also created a class for that, with the function of locating the center of the ball. Also, I created an animation to represent the moment in when the ball is hit. Started working in the scenes, still working to have a few more based on what I have now

--- sketch.js
+++ sketch.js
@@ -1,46 +1,127 @@
-// GLOW EFECT: 
-// https://editor.p5js.org/jesse_harding/sketches/WpONQ8o6u -----> Fill for balls, glow effect
-// maybe check drawingContext in p5.js reference 
+// GLOW EFFECT:
+// https://editor.p5js.org/jesse_harding/sketches/WpONQ8o6u
 
-// VARIABLES IN SKETCH
-// define: ball color and ball "light"
-// background color
+let scene = 0; // (scene0: ball scene, scene1: animation, scene2: back to ball)
+let sceneTimer = 0; // -----> timer to control transitions
+
+let ball;
+let enemy1;
 
 function setup(){
 
   createCanvas(800, 800);
+  ball = new MainBall(width / 2, height / 2, 80, 80);     // -----> to initialice main character
+  enemy1 = new Enemy(random(width), random(height));     // -----> enemie starts in random position in screen
   
-
-}
+} 
 
 function draw(){
- 
-  background(255);
-  ball (100,100);
-  ball (200, 200);
-  mainBall();
+
+  if (scene === 0) {
+    background(255); 
+    enemy1.move();
+    enemy1.display();
+    ball.display();
+
+    checkHit(enemy1, ball);
+  } else if (scene === 1) {
+    hurtAnimation(ball.x, ball.y); // -----> lines from ball's position
+
+    if (millis() - sceneTimer > 1000) {
+      scene = 2;
+      sceneTimer = millis(); // -----> reset timer
+    }
+
+  } else if (scene === 2) {
+    background(255);
+    ball.reduceGlow();
+    ball.display();
+  }
 
 }
 
-function mainBall(){
+class MainBall {     // -----> 'main character'
+
+  constructor (x, y, size, glow){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.glow = glow;
+  }
+
+  display() {
+    fill(0,0,255,10);  
+    for(let i = 0; i < this.glow; i++){    // -----> glow effect
+      ellipse(this.x, this.y, i*2); 
+    }
+    stroke(255);    // ----> stroke and background color must be the same 
+    strokeWeight(2);
+    ellipse(this.x, this.y, this.size); // ----> stroke to define the ball
+  }
+
+  reduceGlow (){   // -----> to reduce glow after each hit (TO FIX)
+    this.glow = max(40, this.glow - 0.1);
+  }
+
+}
+
+class Enemy {     // -----> red triangles that will attack the ball
+
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.speed = random (0.5, 1);
+  }
+
+  move() {
+    let dirX = (width/2 - this.x) * 0.01;
+    let dirY = (height/2 - this.y) * 0.01;
+    this.x += dirX * this.speed;
+    this.y += dirY * this.speed;
+  }
+
+  display() {
+    noStroke();
+    fill (255, 0, 0);
+    triangle(this.x-40, this.y+20, this.x-40, this.y-20, this.x, this.y);
+  }
+
+}
+
+function checkHit(enemy, ball) {
+
+  let d = dist(enemy.x, enemy.y, ball.x, ball.y);
+
+  if (d < ball.size/2) {
+    hit = true;
+    scene = 1; // -----> for switching to hurt animation
+    sceneTimer = millis(); // -----> Start timer
+  }
+
+}
+
+function hurtAnimation(centerX, centerY) { 
+
+  background(0); 
+  stroke(255, 0, 0, 150);
+  strokeWeight(random(0.5, 2)); 
+
+  for (let i = 0; i < 100; i++) {     // -----> random red lines
+    let angle = random(TWO_PI); 
+    let length = random(50, 800); 
+    let x2 = centerX + cos(angle) * length;
+    let y2 = centerY + sin(angle) * length;
+    line(centerX, centerY, x2, y2); 
+  }
+
+}
+
 
   
-  fill(255,0,0,10);  // ----> alpha channel to "dim" the light of the ball (transform into a variable)
-  for(i = 0; i < 50; i++){
-  ellipse(mouseX, mouseY, i*2); 
-  }
-  stroke(255);    // ----> stroke and background color must be the same to create the effect
-  strokeWeight(2);
-  ellipse(mouseX, mouseY, 50, 50); // ----> stroke to define the ball
 
-}
 
-function ball(x, y){   // copy of main ball changing x and ypos and color to draw different balls around
-  
-  fill(0,255,0,10);  
-  for(i = 0; i < 50; i++){
-  ellipse(x, y, i*2); 
-  }
-  ellipse(x, y, 50, 50); 
 
-}
+
+
+
+
